917132f53ec5a6524fafb2073f957d6c26313dc1
kentcb <none@none.com>
2007-12-21 20:01:26 +0000

    - checking in initial revision (V1.0.0.0)

---
+++ Src/Kent.Boogaart.Converters.Examples/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace Kent.Boogaart.Converters.Examples
+{
+	public partial class MainWindow : Window
+	{
+		public static readonly DependencyProperty DateTimeUtcProperty = DependencyProperty.Register("DateTimeUtc",
+			typeof(DateTime),
+			typeof(MainWindow));
+
+		public DateTime DateTimeUtc
+		{
+			get
+			{
+				return (DateTime) GetValue(DateTimeUtcProperty);
+			}
+			set
+			{
+				SetValue(DateTimeUtcProperty, value);
+			}
+		}
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			_comboBox1.ItemsSource = Enum.GetValues(typeof(UriFormat));
+			_textBox7.Text = DateTime.Now.ToString("g");
+			_genderComboBox.ItemsSource = Enum.GetValues(typeof(Gender));
+		}
+
+		private void updateDateTime_Click(object sender, EventArgs e)
+		{
+			DateTimeUtc = DateTime.UtcNow;
+		}
+	}
+}
