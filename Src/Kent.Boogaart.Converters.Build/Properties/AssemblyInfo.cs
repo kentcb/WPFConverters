8e59e22a392692e6c056b82ce11862494a42d1ad
kentcb <none@none.com>
2007-12-21 20:01:26 +0000

    - checking in initial revision (V1.0.0.0)

---
+++ Src/Kent.Boogaart.Converters.Build/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WPF Converters Build")]
+[assembly: AssemblyDescription("Build support for the WPF Converters.")]
+[assembly: AssemblyCompany("Kent Boogaart")]
+[assembly: AssemblyProduct("WPF Converters")]
+[assembly: AssemblyCopyright("© Copyright. Kent Boogaart.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
---
+++ Src/Kent.Boogaart.Converters.Examples/Gender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kent.Boogaart.Converters.Examples
+{
+	public enum Gender
+	{
+		Male,
+		Female
+	}
+}
