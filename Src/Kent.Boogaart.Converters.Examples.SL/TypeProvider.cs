1e749072742edcb2dc871f235f664de9e0d58ae4
kentcb <none@none.com>
2011-09-01 18:33:22 +0000

    - silverlight support - nuget support - switching from NUnit to xUnit - switching from RhinoMocks to Moq - moving documentation into SHFB project - build enhancements - upgrading helper trinity version

---
+++ Src/Kent.Boogaart.Converters.Examples.SL/TypeProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kent.Boogaart.Converters.Examples.SL
+{
+    public sealed class TypeProvider
+    {
+        public Type DateTimeType
+        {
+            get { return typeof(DateTime); }
+        }
+    }
+}
