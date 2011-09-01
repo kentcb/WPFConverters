f7e86466a73a650f26ec7b7f4805953711c346d6
kentcb <none@none.com>
2011-09-01 18:33:22 +0000

    - silverlight support - nuget support - switching from NUnit to xUnit - switching from RhinoMocks to Moq - moving documentation into SHFB project - build enhancements - upgrading helper trinity version

---
+++ Src/Kent.Boogaart.Converters.Examples.SL/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: AssemblyTitle("WPF Converters Examples (Silverlight)")]
+[assembly: AssemblyDescription("Examples for the WPF converters, in Silverlight.")]
+[assembly: AssemblyCompany("Kent Boogaart")]
+[assembly: AssemblyProduct("WPF Converters")]
+[assembly: AssemblyCopyright("© Copyright. Kent Boogaart.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
