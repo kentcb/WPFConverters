70fadf2324756d77244a73accaa4223de2ce9841
kentcb <none@none.com>
2011-09-01 18:33:22 +0000

    - silverlight support - nuget support - switching from NUnit to xUnit - switching from RhinoMocks to Moq - moving documentation into SHFB project - build enhancements - upgrading helper trinity version

---
+++ Src/Kent.Boogaart.Converters/CharacterCasing.cs
@@ -0,0 +1,25 @@
+﻿namespace Kent.Boogaart.Converters
+{
+#if SILVERLIGHT
+    /// <summary>
+    /// Defines different casing modes supported by the <see cref="CaseConverter"/>.
+    /// </summary>
+    public enum CharacterCasing
+    {
+        /// <summary>
+        /// No conversion performed.
+        /// </summary>
+        Normal,
+
+        /// <summary>
+        /// Characters are converted to lowercase.
+        /// </summary>
+        Lower,
+
+        /// <summary>
+        /// Characters are converted to uppercase.
+        /// </summary>
+        Upper
+    }
+#endif
+}
